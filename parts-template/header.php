22cd221a02b870b515471b1933f6c70334a7e09e
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2022-12-19 16:03:00 +0300

    header/nave'

--- parts-template/header.php
+++ parts-template/header.php
@@ -1,4 +1,23 @@
 <?php
-  echo("<header>");
-
-  echo("</header>");
+  // this is header
+?>
+  <header>
+      <div><img width="240px" src="../modern-letter-m-logo-removebg-preview.png" alt="" ></div>
+      <div class="nav-container">
+        <nav>
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">SEVICE</a></li>
+          </ul>
+        </nav>
+        <nav class="right-float">
+          <ul>
+            <li><a href="#">REGISTER</a></li>
+            <li><a href="#">LOGIN</a></li>
+            <li><i class="material-icons" style="color:white">person</i></li>
+          </ul>
+        </nav>
+      </div>
+  </header>
