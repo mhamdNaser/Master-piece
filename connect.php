f272c194df37be95f9724d3c0c67beac9c1a6b71
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2022-12-19 12:15:45 +0300

    header

--- connect.php
+++ connect.php
@@ -1,16 +1,2 @@
 <?php
-// Connect to the database
-// $db = mysqli_connect('localhost', 'first_name', 'last_name', 'e_mail', 'phone_number', 'id_number', 'birth_day', 'gender', 'type', '	user_name' , 'password', 'photo');
-$host = "localhost";
-$username = "naser";
-$password = "naser2022";
-$dbname = "users";
 
-// Create connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
--- index.php
+++ index.php
@@ -4,16 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Golden House</title>
 </head>
 <body>
-    <header>
-        <?php
-        ?>
-    </header>
-        <?php include("./connect.php");
-              include("./parts-template/register.php");
+    <?php include("./parts-template/header.php"); ?>
+    <?php include("./connect.php");
              // include("./parts-template/login.php");
              ?>
     <script src="script.js"></script>
