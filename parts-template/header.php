8e1a19eedb921e61928bd4194a2b1dcf0e107e55
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2022-12-20 12:06:17 +0300

    error

--- parts-template/header.php
+++ parts-template/header.php
@@ -1,23 +1,20 @@
 <?php
   // this is header
 ?>
-  <header>
-      <div><img width="240px" src="../modern-letter-m-logo-removebg-preview.png" alt="" ></div>
-      <div class="nav-container">
-        <nav>
-          <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">CONTACT</a></li>
-            <li><a href="#">SEVICE</a></li>
-          </ul>
-        </nav>
-        <nav class="right-float">
-          <ul>
-            <li><a href="#">REGISTER</a></li>
-            <li><a href="#">LOGIN</a></li>
-            <li><i class="material-icons" style="color:white">person</i></li>
-          </ul>
-        </nav>
+<header>
+  <nav>
+    <ul>
+      <div class="left-float">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li><a href="#">SEVICE</a></li>
       </div>
-  </header>
+      <div class="right-float">
+        <li><a href="#">REGISTER</a></li>
+        <li><a href="#">LOGIN</a></li>
+        <li><a href="#"><i class="material-icons" style="color:white">person</i></a></li>
+      </div>
+    </ul>
+  </nav>
+</header>
