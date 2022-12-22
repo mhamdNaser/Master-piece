1c19cdfa01acff2a9f7c7ca2248df7b241223b09
unknown <mhamd.naser.edden@gmail.com>
2022-12-22 20:42:51 +0300

    insert.php

--- db/insert.php
+++ db/insert.php
@@ -2,4 +2,31 @@
 
     require_once("db/config.php");
 
-    $connect_db = "INSERT FROM users VALUE ('')"
+    $type = $_REQUEST['type'];
+    $fi_name = $_REQUEST['first_name'];
+    $la_name = $_REQUEST['last_name'];
+    $pass = $_REQUEST['password'];
+    $email = $_REQUEST['email'];
+
+    if ($connect_db->query($sql) === TRUE) {
+        echo "record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if($type == "Student"){
+            $connect_db = "INSERT INTO student ('st_id','st_fname','st_lname','st_password','st_email') VALUES ('0', $fi_name, $la_name, $pass, $email)";
+            require_once("./index.php");
+        }
+        elseif($type == "Partner"){
+            $connect_db = "INSERT INTO student ('st_id','st_fname','st_lname','st_password','st_email') VALUE ('0', $fi_name, $la_name, $pass, $email)";
+            require_once("./index.php");
+        }
+        else{
+            echo "ERROR", mysqli_connect_error();
+        }
+
+    }
+
+    
--- parts-template/register.php
+++ parts-template/register.php
@@ -1,14 +1,22 @@
 <?php
     //registeration page 
 ?>
-<form id="register" action="./db/config.php" method="POST" >
+<form id="register" action="./db/config.php" method="post" >
+    <label for="">Official Status</label>
+    <br>
+    <select name="type">
+        <option></option>
+        <option>Student</option>
+        <option>Partner</option>
+    </select>
+    <br>
     <label for="">first name</label>
-    <input type="text" name="" value="" placeholder="first name">
+    <input type="text" name="first_name" value="" placeholder="first name">
     <label for="">last name</label>
-    <input type="text" name="" value="" placeholder="last name">
+    <input type="text" name="last_name" value="" placeholder="last name">
     <label for="">password</label>
-    <input type="text" name="" value="" placeholder="password">
+    <input type="text" name="password" value="" placeholder="password">
     <label for="">email</label>
-    <input type="email" name="" value="" placeholder="email">
+    <input type="email" name="email" value="" placeholder="email">
     <button type="submit" >Regiser</button>
 </form>
