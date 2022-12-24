97a0acf78b9ecf9d0c28467ece75f6a4ee0fd6e4
unknown <mhamd.naser.edden@gmail.com>
2022-12-24 17:30:17 +0300

    insert is done

--- db/config.php
+++ db/config.php
@@ -3,24 +3,19 @@
 $hostName = "localhost";
 $DBname = "users1";
 $DBuser = "root";
-$UserPassword = '';
+$UserPassword = "";
 
-$connection_DB = mysqli_connect($hostName,$DBuser,$UserPassword,$DBname);
-
-// // check connection 
-// if(!$connection_DB){
-//     echo("CONNECTION ERROR" . mysqli_connect_error());
-// }
-// else{
-//     echo("DataBase IS Connected");
-// }
+$connect_db = mysqli_connect($hostName,$DBuser,$UserPassword,$DBname);
 
 try {
-    $connection_DB= new PDO("mysql:host=$hostName;dbname=$DBname", $DBuser, $UserPassword);
-    $connection_DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h4>DataBase is Connected successfully.</h4>";
+    // connect the database
+    $connect_db= new PDO("mysql:host=$hostName;dbname=$DBname", $DBuser, $UserPassword);
 
-    } catch(PDOException $e) {
+    // message the connect is true
+    $connect_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<h4>DataBase is Connected successfully.</h4>";
+}
+catch(PDOException $e) {
 
         echo 'ERROR: ' . $e->getMessage();
     }
--- db/insert.php
+++ db/insert.php
@@ -2,22 +2,6 @@
 
     require_once("./config.php");
     
-//     if($_SERVER["REQUEST_METHOD"] === "POST"){
-
-//         $fi_name = $_REQUEST['first_name'];
-//         $la_name = $_REQUEST['last_name'];
-//         $pass = $_REQUEST['password'];
-//         $email = $_REQUEST['email'];
-
-
-//         $sql = "INSERT INTO student (st_id,st_fname, st_lname, st_password, st_email) VALUES ('0', '$fi_name', '$la_name', '$pass', '$email')";
-
-//         echo  "<br>", $fi_name ,"<br>", $la_name ,"<br>", $pass ,"<br>", $email;
-        
-//         // Close connection
-//         mysqli_close($connection_DB);
-
-// }
 
 try
     {
@@ -27,15 +11,17 @@ try
         $pass = $_REQUEST['password'];
         $email = $_REQUEST['email'];
 
-        $sql = "INSERT INTO student (st_id,st_fname, st_lname, st_password, st_email) VALUES ('0', '$fi_name', '$la_name', '$pass', '$email')";
+        // insert value in database
+        $sql = "INSERT INTO student (id, fname, lname, password, email) VALUES ('', '$fi_name', '$la_name', '$pass', '$email')";
+        $connect_db->exec($sql);
 
-        $$connection_DB->exec($sql);
+        // message the insert is done
         echo "<h3>data stored in a database successfully.</h3>";
     }
     catch(PDOException $e){
             echo "ERROR: Hush! Sorry. " ;
     }
         
-        // Close connection
-        $connection_DB = null;
-        echo("<a href='../index.php'>HOME</a>");
+    // Close connection
+    $connect_db = null;
+    echo("<a href='../index.php'>HOME</a>");
